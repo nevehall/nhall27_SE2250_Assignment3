b5a830158cee6e71465ba0f68e1a1a1552dad3b1
Neve Hall <nhall27@uwo.ca>
2018-03-21 21:25:36 -0400

    Initial commit

---
+++ Assets/__Scripts/Twitter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Twitter: MonoBehaviour {
+
+
+	public string twitterNameParameter = "Check out my score!";
+	private const string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+	private const string TWITTER_LANGUAGE = "en";
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void pressed(){
+		Application.OpenURL(TWITTER_ADDRESS+"?text="+WWW.EscapeURL(twitterNameParameter+"\n"+ScoreManager.HIGH_SCORE+"\n"));
+
+	}
+}
